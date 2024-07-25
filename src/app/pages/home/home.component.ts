d336bb7a018f7c0c7612b14839ee8975f7ae57f9
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2024-07-25 13:46:30 -0300

    created the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from "../navigation/navigation.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
